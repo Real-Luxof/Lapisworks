1a616a5e05678bc3a88c2880dddd4b37258541fa
Real-Luxof <luxtheluxof@gmail.com>
2026-05-29 07:07:51 +0500

    goodbye unreliable mixin

--- src/main/java/com/luxof/lapisworks/mixin/ServerWorldMixin.java
+++ src/main/java/com/luxof/lapisworks/mixin/ServerWorldMixin.java
@@ -3,25 +3,29 @@ package com.luxof.lapisworks.mixin;
 import at.petrak.hexcasting.api.block.circle.BlockCircleComponent;
 
 import com.luxof.lapisworks.init.PersistentStateCircleBlockCache;
+import com.luxof.lapisworks.media.LinkableMediaBlock;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerWorld.class)
 public class ServerWorldMixin {
+    @Unique private ServerWorld world = (ServerWorld)(Object)this;
+
     @Inject(method = "onBlockChanged", at = @At("HEAD"))
     public void lapisworks$updateMyPersistentState(
         BlockPos pos, BlockState oldState, BlockState newState, CallbackInfo ci
     ) {
         if (oldState.isOf(newState.getBlock())) return;
 
-        var persistentState = PersistentStateCircleBlockCache.getState((ServerWorld)(Object)this);
+        var persistentState = PersistentStateCircleBlockCache.getState(world);
         if (oldState.getBlock() instanceof BlockCircleComponent &&
             !(newState.getBlock() instanceof BlockCircleComponent)) {
 
@@ -35,5 +39,15 @@ public class ServerWorldMixin {
             persistentState.markDirty();
 
         }
+
+        if (
+            !oldState.hasBlockEntity() ||
+            !(world.getBlockEntity(pos) instanceof LinkableMediaBlock lmb)
+        )
+            return;
+        
+        lmb.getLinks().forEach(
+            linkedPos -> ((LinkableMediaBlock)world.getBlockEntity(linkedPos)).removeLink(pos)
+        );
     }
 }
